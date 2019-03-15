05b7243f6b6d9791bf9e7f1a011a08001024acac
ErosPingani1 <e.pingani3@gmail.com>
2019-03-15 10:57:58 +0100

    Commento test

--- src/app/catalogopremi/tabs/catalogo/catalogo.page.spec.ts
+++ src/app/catalogopremi/tabs/catalogo/catalogo.page.spec.ts
@@ -15,6 +15,7 @@ describe('CatalogoPage', () => {
     .compileComponents();
   }));
 
+  //Qua si spaccava
   beforeEach(() => {
     fixture = TestBed.createComponent(CatalogoPage);
     component = fixture.componentInstance;
