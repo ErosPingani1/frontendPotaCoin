27ec4bfed8b87f37ad6dc92e81d302e2b582436c
EmanueleZucchetti <36767243+EmanueleZucchetti@users.noreply.github.com>
2019-03-20 12:25:02 +0100

    layout da riscattare e utilizzati

--- src/app/mieibuoni/tabs/dariscattare/dariscattare.page.ts
+++ src/app/mieibuoni/tabs/dariscattare/dariscattare.page.ts
@@ -1,15 +1,77 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Component({
-  selector: 'app-dariscattare',
-  templateUrl: './dariscattare.page.html',
-  styleUrls: ['./dariscattare.page.scss'],
+    selector: 'app-dariscattare',
+    templateUrl: './dariscattare.page.html',
+    styleUrls: ['./dariscattare.page.scss'],
 })
 export class DariscattarePage implements OnInit {
+    vauchers: any = [];
+    constructor(private http: HttpClient) {
 
-  constructor() { }
+    }
 
-  ngOnInit() {
-  }
+    creaDaJson() {
 
+        //Creazione di lista di vaucher vuota
+        console.log("prova")
+
+        //ForEach che salva il valore del json nella lista
+        fetch('assets/json/PremiMockati.json')
+            .then(r => r.json())
+            .then(j => {
+                console.log(j);
+                console.log(j);
+                for (let i of j) {
+                    this.vauchers.push(i);
+
+                }
+            });
+
+        console.log(this.vauchers);
+
+    }
+
+    ngOnInit() {
+        this.creaDaJson();
+        this.getAllBuoni();
+    }
+
+    getAllBuoni() {
+        this.http.get('http://localhost:9090/potacoin/potacoinbackend/buoni/all', {
+            headers:
+                new HttpHeaders(
+                    {
+                        'Content-Type': 'application/json',
+                        'X-Requested-With': 'HttpRequest',
+                        'MyClientCert': '',        // This is empty
+                        'MyToken': ''              // This is empty
+                    }
+                )
+        }).subscribe((response) => {
+            console.log(response);
+        });
+
+        //Creazione della classe Esercente in modo da poter creare degli oggetti esercenti nel component
+
+
+    }
+}
+
+export class Vaucher {
+
+    id_buono: number;
+    tipo_buono: string;
+    scadenza_assoluta: string;
+    scadenza: number;
+    esercente: any;
+
+    constructor(id_buono: number, tipo_buono: string, scadenza_assoluta: string, scadenza: number, esercente: any) {
+        this.id_buono = id_buono;
+        this.tipo_buono = tipo_buono;
+        this.scadenza_assoluta = scadenza_assoluta;
+        this.scadenza = scadenza;
+        this.esercente = esercente;
+    }
 }
